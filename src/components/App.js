5c28571e8b2e526b4a455d1ec9a19875e79c8259
QuanDev2 <qnguyen.dev2@gmail.com>
2020-11-28 01:28:48 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+// import './components/App.css';
+import NavBar from './utils/NavBar'
+// import SocialMediaPole from './components/utils/SocialMediaPole.js'
+// import HomePage from './components/homePage/HomePage.js'
+// import AboutMe from './components/aboutMe/AboutMe.js'
+// import Portfolio from './components/portfolio/Portfolio.js'
+// import Resume from './components/resume/Resume.js'
+// import Contact from './components/contact/Contact.js'
+// import Footer from './components/utils/Footer.js'
+
+
+function App() {
+  return (
+    <div className="app">
+      {/* Nav bar */}
+      <NavBar />
+      
+      {/* Social media pole */}
+      {/* <SocialMediaPole /> */}
+
+      {/* homepage */}
+      {/* <HomePage /> */}
+
+      {/* about me */}
+      {/* <AboutMe /> */}
+
+      {/* portfolio */}
+      {/* <Portfolio /> */}
+
+      {/* resume */}
+      {/* <Resume /> */}
+
+      {/* contact */}
+      {/* <Contact /> */}
+
+      {/* footer */}
+      {/* <Footer />  */}
+
+    </div>
+  );
+}
+
+export default App;
