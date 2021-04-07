62666d4e56510d8242879238c9ff2e53d69b0fce
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-07 01:55:33 -0700

    updated resume to new format 3.0.1

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,18 +1,23 @@
 import React from 'react'
 // import './Resume.css'
 
-
 function Resume() {
-  return (
-    <div id="Resume">
-      <h1>My resume</h1>
-      <div className="Resume__Frame">
-
-        <iframe src="https://drive.google.com/file/d/1AYTy6W8Axl9b_mmydf9Kopj3U01Dh5O6/preview?usp=sharing&embedded=true" frameborder="0"></iframe>
-      </div>
-
-    </div>
-  )
+	return (
+		<div id="Resume">
+			<h1>My resume</h1>
+			<div className="Resume__Frame">
+				<iframe
+					src="https://drive.google.com/file/d/1WKEN4LsqoMOmZGYyiDJoN4vGQCBCA0x5/preview"
+					width="640"
+					height="480"
+				></iframe>
+				{/* <iframe
+					src="https://drive.google.com/file/d/1WKEN4LsqoMOmZGYyiDJoN4vGQCBCA0x5/view?usp=sharing"
+					frameborder="0"
+				></iframe> */}
+			</div>
+		</div>
+	)
 }
 
 export default Resume
