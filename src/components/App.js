fc25e9208f0b91ebc0ffbf82591547141cd57de0
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-29 17:40:34 -0700

    added loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,59 @@
 import './App.css';
-import HomePage from './homePage/HomePage'
-import AboutMe from './aboutMe/AboutMe'
+import HomePage from './homePage/HomePage';
+import AboutMe from './aboutMe/AboutMe';
 import Portfolio from './portfolio/Portfolio';
 import Resume from './resume/Resume';
 import Contact from './contact/Contact';
 import Header from './utils/Header';
 import SocialMediaPole from './utils/SocialMediaPole';
+import { useState, useEffect } from 'react';
+import HashLoader from 'react-spinners/HashLoader';
+import testUtils from 'react-dom/test-utils';
 
 function App() {
-  document.title = 'Quan Nguyen';
-  return (
-    <div className="app">
-      {/* Header */}
-      <Header />
+	document.title = 'Quan Nguyen';
+	const [loading, setLoading] = useState(true);
 
-      {/* Social media pole */}
-      <SocialMediaPole />
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 2500);
+	}, []);
 
-      {/* homepage */}
-      <HomePage />
+	return (
+		<>
+			{loading ? (
+				<div className="loader">
+					<HashLoader color="#28a498" size={150}/>
+				</div>
+			) : (
+				<div className="app">
+					{/* Header */}
+					<Header />
 
-      {/* about me */}
-      <AboutMe />
+					{/* Social media pole */}
+					<SocialMediaPole />
 
-      {/* portfolio */}
-      <Portfolio />
+					{/* homepage */}
+					<HomePage />
 
-      {/* Resume */}
-      <Resume />
+					{/* about me */}
+					<AboutMe />
 
-      {/* Contact page */}
-      <Contact />
+					{/* portfolio */}
+					<Portfolio />
 
-      {/* Footer */}
+					{/* Resume */}
+					<Resume />
 
-    </div>
-  );
+					{/* Contact page */}
+					<Contact />
+
+					{/* Footer */}
+				</div>
+			)}
+		</>
+	);
 }
 
 export default App;
